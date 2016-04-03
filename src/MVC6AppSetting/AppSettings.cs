6e98cb667d17f981a6ebce7e9c3630af93efff06
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-04-04 00:12:40 +0530

    Initial commit

---
+++ src/MVC6AppSetting/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC6AppSetting
+{
+    public class AppSettings
+    {
+        public string WebSiteURL { get; set; }
+        public string WebSiteTitle { get; set; }
+    }
+}
---
+++ src/MVC6AppSetting/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+
+namespace MVC6AppSetting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _webSiteURL;
+        private readonly string _webSiteTitle;
+        public HomeController(IOptions<AppSettings> settings)
+        {
+            _webSiteTitle = settings.Value.WebSiteTitle;
+            _webSiteURL = settings.Value.WebSiteURL;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.WebSiteTitle = _webSiteTitle;
+            ViewBag.WebSiteURL = _webSiteURL;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
